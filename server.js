81fd48f74e8f8990c64809d147bd893dc02d759e
Nina Kim <neenerskim@gmail.com>
2021-09-29 14:11:55 -0700

    complete budgtr express lab

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const app = express()
+
+const Budget = require('./models/budget.js')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get('/budgets', (req, res) => {
+    res.render('index.ejs', {
+        allBudgets: Budget,
+    })
+})
+
+app.get('/budgets/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+app.post('/budgets', (req, res) => {
+    console.log(req.body);
+    let tags = req.body.tags
+
+    const newBudget = {
+        date: req.body.date,
+        name: req.body.name,
+        from: req.body.from,
+        amount: req.body.amount,
+        tags: tags.split(', ')
+    }
+  
+    Budget.push(newBudget);
+    res.redirect('/budgets');
+  });
+
+app.get('/budgets/:id', (req, res) => {
+    console.log(req.params.id)
+    res.render('show.ejs', {
+        budget: Budget[req.params.id]
+    })
+})
+
+
+
+app.listen(3000, () => {
+    console.log('listening...')
+})
